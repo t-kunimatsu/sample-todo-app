b890e3a5fdd720f89a23883be40f4213f4e4ba3f
t_kunimatsu <>
2024-06-08 01:06:10 +0900

    use typescript

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -1,11 +1,12 @@
-import { useState } from 'react';
+import { useState, PropsWithChildren, ReactNode } from 'react';
 import ApplicationLogo from '@/Components/ApplicationLogo';
 import Dropdown from '@/Components/Dropdown';
 import NavLink from '@/Components/NavLink';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
 import { Link } from '@inertiajs/react';
+import { User } from '@/types';
 
-export default function Authenticated({ user, header, children }) {
+export default function Authenticated({ user, header, children }: PropsWithChildren<{ user: User, header?: ReactNode }>) {
     const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
 
     return (
@@ -99,7 +100,9 @@ export default function Authenticated({ user, header, children }) {
 
                     <div className="pt-4 pb-1 border-t border-gray-200">
                         <div className="px-4">
-                            <div className="font-medium text-base text-gray-800">{user.name}</div>
+                            <div className="font-medium text-base text-gray-800">
+                                {user.name}
+                            </div>
                             <div className="font-medium text-sm text-gray-500">{user.email}</div>
                         </div>
 
