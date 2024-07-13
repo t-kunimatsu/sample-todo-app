e5a300a5f62b7557f301d375f8cea7da014c1c8f
ballas-kunimatsu <toshihiko.kunimatsu@ballasx.com>
2024-07-13 23:30:42 +0900

    testing-library, vitest, mswなどを追加

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.tsx',
-            refresh: true,
-        }),
-        react(),
-    ],
+  plugins: [
+    laravel({
+      input: "resources/js/app.tsx",
+      refresh: true,
+    }),
+    react(),
+  ],
+  test: {
+    globals: true,
+  },
 });
